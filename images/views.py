0f5e2c6b5033b4761f7fbd3d581ec369053a30f4
mohammedsab <mo7ammedsab@gmail.com>
2023-01-18 13:14:42 +0200

    ajax with like button and scroll in image page

--- images/views.py
+++ images/views.py
@@ -2,13 +2,47 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.shortcuts import get_object_or_404
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.decorators.http import require_POST
+
+from common.decorators import ajax_required
 
 from .models import Image
 from .forms import ImageCreateForm
 
 # Create your views here.
+
+
+def is_ajax(request):
+    return request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
+
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    
+
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        images = paginator.page(1)
+    except EmptyPage:
+        if is_ajax(request=request):
+            # If the request is AJAX and the page is out of range
+            # return an empty page
+            return HttpResponse('')
+        # If page is out of range deliver last page of results
+        images = paginator.page(paginator.num_pages)
+
+    if is_ajax(request=request):
+        return render(request, 'images/image/list_ajax.html',
+                      {'section': 'images', 'images': images})
+    return render(request, 'images/image/list.html',
+                  {'section': 'images', 'images': images})
 
 
 @login_required
@@ -39,6 +73,7 @@ def image_detail(request, id, slug):
                   {'section': 'images', 'image': image})
 
 
+@ajax_required
 @login_required
 @require_POST
 def image_like(request):
@@ -47,13 +82,13 @@ def image_like(request):
     if image_id and action:
         try:
             image = Image.objects.get(id=image_id)
-            if action=='like':
+            if action == 'like':
                 image.users_like.add(request.user)
 
             else:
                 image.users_like.remove(request.user)
-            return JsonResponse({'status':'ok'})
+            return JsonResponse({'status': 'ok'})
         except:
             pass
 
-    return JsonResponse({'status':'error'})
+    return JsonResponse({'status': 'error'})
