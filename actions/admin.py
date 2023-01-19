be12a449b158a8b5bc22dd1bf2f394e957efebd0
mohammedsab <mo7ammedsab@gmail.com>
2023-01-19 23:46:27 +0200

    adding action models and create_action in utils.py

--- actions/admin.py
+++ actions/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from .models import Action
+
 # Register your models here.
+
+@admin.register(Action)
+class ActionAdmin(admin.ModelAdmin):
+    '''Admin View for Action'''
+
+    list_display = ('user','verb', 'target', 'created')
+    list_filter = ('created',)
+    # inlines = [
+    #     Inline,
+    # ]
+    # raw_id_fields = ('',)
+    # readonly_fields = ('',)
+    search_fields = ('verb',)
+    # date_hierarchy = ''
+    # ordering = ('',)
--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1']
 INSTALLED_APPS = [
     'account.apps.AccountConfig',
     'images.apps.ImagesConfig',
+    'actions.apps.ActionsConfig',
 
     'social_django',
     'django_extensions',
